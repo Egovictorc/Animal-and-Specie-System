055e9d6e6a54b943e2d7b9dfdd8d890c1ccf0df4
EGO VICTOR <egovictorc@outlook.com>
2019-05-31 07:55:47 +0100

    add validation

--- js/admin.js
+++ js/admin.js
@@ -1,44 +1,111 @@
 $(document).ready(function() {
-    // FORM POST
+  // FORM POST
   let admin = {
     email: $("input#email").val(),
     password: $("input#password").val()
   };
-  // $("#signup").on("click", function(e) {
-  $("form").submit(function(e) {
+  // SIGN UP
+  $("form#signupform").submit(function(e) {
     e.preventDefault();
 
+    // GET ADMIN INFO
     let addAdmin = {
-        firstName: $("input#first-name").val(),
-        lastName: $("input#last-name").val(),
-        email: $("input#email").val(),
-        password: $("input#password").val(),
-        phoneNumber: $("input#phone").val(),
-      }
-    let admin = {
-        email: $("input#email").val(),
-        password: $("input#password").val(),
-    }
-    let url ="";
-    if (  $(this).id === "signupform" ) {
-        data = addAdmin;
-        url = "http://localhost:3000/new-admin";
-    } else if ($(this).id === "signinform") {
-        data = admin;
-        url = "http://localhost:3000/admin";
-    }
-    // let form =  $(this.id)
-    // let data = ( form === "signup" ) ? addAdmin : ;
-   
+      firstName: $("input#first-name").val(),
+      lastName: $("input#last-name").val(),
+      email: $("input#email").val(),
+      password: $("input#password").val(),
+      phoneNumber: $("input#phone").val()
+    };
 
-    $.post( "http://localhost:3000/admin", addAdmin, function(data, obj, success) {
-        // alert(JSON.stringify(success));
-        window.location.href = "../pages/admin.html"
-        console.log(data);
-      })
+    // VALIDATE THAT ADMIN HAVE NOT REGISTERED BEFORE
+    let isRegistered = false;
+    let registerCount = 0;
+    let registerArray = [];
+    $.getJSON("http://localhost:3000/new-admin", function(admins) {
+      $.each(admins, function(index, value) {
+          registerArray.push(value.email)
+        if (value[email] === addAdmin.email) {
+          registerCount = registerCount + 1;  
+          console.log(value[email])
+        }
+      });
+      // isRegistered = ( registerCount === 0 ) ? false : true;
     });
 
+    for (let val of registerArray){
+      if(val === addAdmin.email) {
+        isRegistered = true
+      }
+    }
 
-})
-   
+    let error = {};
+    let errorCount = 0;
 
+    for (let [key, val] of Object.entries(addAdmin)) {
+      if (!val) {
+        error[key] = "Please fill all input fields";
+        errorCount++;
+      }
+    }
+    if (isRegistered) {
+      alert(`You have registered before, Please sign in`);
+    } else if (errorCount === 0 && ( isRegistered === false) ) {
+      $.post("http://localhost:3000/new-admin", addAdmin, function(
+        data,
+        obj,
+        success
+      ) {
+      
+        alert(`Your registration was successful, you can now signin`);
+        // window.location.href = "../pages/admin.html"
+        // console.log(data);
+        
+        // $("form:input").attr("value", "");
+        // $("input#first-name").attr("value", "");
+        // $("input#last-name").attr("value", "");
+        // $("input#email").attr("value", "");
+        // $("input#password").attr("value", "");
+        // $("input#phone").attr("value", "");
+      });
+      // $(this).closest("form").find('input[type]').val("");
+      
+    } else {
+      alert(`Please fill all input fields`);
+      alert(addAdmin.email);
+    }
+    
+  });
+
+  // SIGIN IN FORM
+  $("form#signinform").submit(function(e) {
+    e.preventDefault();
+
+    let admin = {
+      email: $("input#passemail").val(),
+      password: $("input#passpassword").val(),
+      date: new Date()
+    };
+
+    let errorCount = 0;
+    let error = {};
+    for (let [key, value] of Object.entries(admin)) {
+      if (!value) {
+        errorCount = errorCount + 1;
+      }
+    }
+
+    if (errorCount === 0) {
+      $.post("http://localhost:3000/admin", admin, function(
+        data,
+        obj,
+        success
+      ) {
+        // alert(JSON.stringify(success));
+        window.location.href = "../pages/admin.html";
+        // console.log(data);
+      });
+    } else {
+      alert(`Please fill all input fields`)
+    }
+  });
+});
--- js/animals.js
+++ js/animals.js
@@ -1,87 +1,170 @@
 $(document).ready(function() {
-    // FORM POST
-    // lat div = "";
+  // FORM POST
+  // lat div = "";
 
-    // <div class="specie__div" >
-    //           <h3 class="family__name">Amphibians</h3>
-    //           <div class="specie__img-cont" id="amph-1">
-    //             <img src="images/giraffe.jpg" alt="" class="specie__img" />
-    //           </div>
-    //           <p class="specie__desc">
-    //             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius
-    //             veritatis, eligendi ut quisquam facilis dicta, nam maxime
-    //             placeat, cumque aspernatur neque quis magnam officiis id
-    //             doloremque! Numquam blanditiis velit labore?
-    //           </p>
-    //         </div>
+  $.getJSON("http://localhost:3000/animals", function(data) {
+    let amphibians = "";
+    let reptiles = "";
 
-
-    $.getJSON("http://localhost:3000/animals", function(data){
-    let amphibians = '';
-    let reptiles = '';
-
-        $.each(data, function(index, value){
-            if( value.specieName === "amphibian"){
-        amphibians+= `<div class='specie__div'> `
-        amphibians+=  `<h3 class="family__name heading"> ${value.specieName} </h3>`
-        amphibians+= `<div class="specie__img-cont" >`
-        amphibians+= `<img src=${value.url} alt=${value.animalName} class="specie__img" />`
-        amphibians+=  "</div>"
-        amphibians+= `<p class="specie__desc">${value.description}</p>`
-        amphibians+= `</div>`
-        } 
-        else {
-        reptiles+= `<div class='specie__div'> `
-        reptiles+=  `<h3 class="family__name heading"> ${value.specieName} </h3>`
-        reptiles+= `<div class="specie__img-cont" >`
-        reptiles+= `<img src=${value.url} alt=${value.animalName} class="specie__img" />`
-        reptiles+=  "</div>"
-        reptiles+= `<p class="specie__desc">${value.description}</p>`
-        reptiles+= `</div>`
-            }
-        })
-        // $("#reptiles").append(reptiles)
-        $("#amphibians").append(amphibians)
-    } )
-
+    $.each(data, function(index, value) {
+      if (value.specieName === "amphibian") {
+        amphibians += `<div class='specie__div'> `;
+        amphibians += `<h3 class="family__name heading"> ${
+          value.animalName
+        } </h3>`;
+        amphibians += `<div class="specie__img-cont" >`;
+        amphibians += `<img src=${value.url} alt=${
+          value.animalName
+        } class="specie__img" />`;
+        amphibians += "</div>";
+        amphibians += `<p class="specie__desc">${value.description}</p>`;
+        amphibians += `</div>`;
+      } else {
+        reptiles += `<div class='specie__div'> `;
+        reptiles += `<h3 class="family__name heading"> ${
+          value.animalName
+        } </h3>`;
+        reptiles += `<div class="specie__img-cont" >`;
+        reptiles += `<img src=${value.url} alt=${
+          value.animalName
+        } class="specie__img" />`;
+        reptiles += "</div>";
+        reptiles += `<p class="specie__desc">${value.description}</p>`;
+        reptiles += `</div>`;
+      }
+    });
+    $("#reptiles").append(reptiles);
+    $("#amphibians").append(amphibians);
+  });
 
   let admin = {
     email: $("input#email").val(),
     password: $("input#password").val()
   };
 
-    // ADD ANIMALS
-  // $("#signup").on("click", function(e) {
-  $("#addbtn").submit(function(e) {
+  // ADD ANIMALS
+//   $("#signup").on("click", function(e) {
+//   $("#signup").click(function(e) {
+  $("button#addbtn").click(function(e) {
     e.preventDefault();
     let error = {};
-    let errorCount = 0;
+    
     let newAnimal = {
-        specieName: $("input#specie-name").val(),
-        familyName: $("input#family-name").val(),
-        animalName: $("input#animal").val(),
-        description: $("input#description").val(),
-        url: $("input#url").val(),
+      specieName: $("input#specie-name").val(),
+      // familyName: $("input#family-name").val(),
+      animalName: $("input#animal").val(),
+      description: $("input#description").val(),
+      url: $("input#url").val()
+    };
+    let errorCount = 0;
+    for (let [key, value] of Object.entries(newAnimal) ) {
+      if (!value) {
+        error[key] = "empty field";
+        errorCount = errorCount + 1;
       }
-    for( let [key, value] of Object.entries(newAnimal) ){
-        if (value === ""){
-            error[key] = "empty field";
+    }
+    // ADD ANIMALS TO DATABASE
+    if(errorCount === 0) {
+    $.post("http://localhost:3000/animals", newAnimal, function(
+      data, obj, success
+    ) {
+    //   window.location.href = "../pages/index.html";
+    alert(`${newAnimal.animalName} was added succesfully`);
+      alert(JSON.stringify(success));
+      // GET ANIMAL FROM
+    });
+    } 
+    else {
+        alert("Please fill all input fields")
+    }
+  });
+
+  
+  //UPDATE ANIMALS
+  $("#updatebtn").click(function(e) {
+    e.preventDefault();
+    let targetAnimal = {
+      specieName: $("input#specie-name").val(),
+      // familyName: $("input#family-name").val(),
+      description: $("input#description").val(),
+      animalName: $("input#animal").val(),
+      id: $("input#id").val(),
+      url: $("input#url").val()
+    };
+
+    // VALIDATE INPUT FIELDS
+    let error = {};
+    let errorCount = 0;
+    for( let [key, value] of Object.entries(targetAnimal)){
+        if( value === "" ) {
+            error[key] = "Empty field"
             errorCount = errorCount + 1;
         }
     }
-    // ADD ANIMALS TO DATABASE 
-    // if(errorCount === 0) {
-        $.post( "http://localhost:3000/animals", newAnimal, function(data, obj, success) {
-        window.location.href = "../pages/admin.html"
-        alert(JSON.stringify(success));
-        // GET ANIMAL FROM 
-      })
-    // } else {
-    //     alert("Please fill all input fields")
-    // }
-    
-    });
-    // DELETE ANIMALS
-})
-   
 
+    if ( targetAnimal.id !== "") {
+      $.ajax({
+        url: `http://localhost:3000/animals/${targetAnimal.id}`,
+        type: "PUT",
+        dataType: "json",
+        data: targetAnimal,
+        success: function() {
+          // window.location.href = "../pages/admin.html"
+        //   alert(targetAnimal.animalName);
+          alert(`updated successfully`);
+        }
+      });
+    } else {
+      alert(`Please fill all input fields`);
+    }
+  });
+
+  // DELETE ANIMALS
+  $("form#deleteAnimal").submit(function(e) {
+    e.preventDefault();
+    let targetAnimal = {
+      // specieName: $("input#specie-name").val(),
+      // familyName: $("input#family-name").val(),
+      animalName: $("input#animal").val(),
+      id: $("input#id").val(),
+      // url: $("input#url").val()
+    };
+    let id = 0;
+    let deleteCount = 0
+    $.getJSON("http://localhost:3000/animals", function(data){
+        $.each(data, function(index, value){
+            console.log(value.id, targetAnimal.id)
+            if( value.id === targetAnimal.id ){
+                id = value.id;
+                deleteCount++
+                // console.log(value.id, id)
+            }
+        })
+    })
+
+    if (targetAnimal.id) {
+      $.ajax({
+        type: "DELETE",
+        url: `http://localhost:3000/animals/${targetAnimal.id}`,
+        success: function() {
+          // window.location.href = "../pages/admin.html"
+          alert(targetAnimal.animalName);
+          alert(`deleted successfully`);
+        }
+      });
+    } 
+    else {
+        // 
+      alert(`Please fill all input fields`);
+    }
+  });
+});
+
+// {
+//     "id": 3,
+//     "animalName": "lion",
+//     "specieName": "reptile",
+//     "familyName": "family",
+//     "url": "images/lion.jpg",
+//     "description": "This is an animal from this family"
+//   }
